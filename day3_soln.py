6415c26d854b619da9465b73b93dd300a5f9bbc0
Sumir Broota <sumirbroota@gmail.com>
2025-01-19 02:08:28 +0530

    day 3 part 2

--- day3_soln.py
+++ day3_soln.py
@@ -12,7 +12,21 @@ def part1():
     print(sum_of_mul_nos)
 
 
+def part2():
+    sum_of_mul_nos = 0
+    number_pairs = []
+    with open("day3_input.txt") as file:
+        full_text = file.readlines(-1)
+        full_text = " ".join(full_text)
+    split_by_do = re.split("do\\(", full_text)
+    for split_line in split_by_do:
+        split_by_dont = re.split("don't\\(", split_line)[0]
+        number_pairs += re.findall("mul\\((\\d{0,3}),(\\d{0,3})\\)", split_by_dont)
+    for pair in number_pairs:
+        sum_of_mul_nos += int(pair[0]) * int(pair[1])
+    print(sum_of_mul_nos)
+
 
 if __name__ == "__main__":
     part1()
-
+    part2()
